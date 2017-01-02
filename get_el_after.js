6b8dd971a5bb8d2fcd3c558be35afd22f2522056
Vannak Chhay <vannak.chhay1@gmail.com>
2017-01-01 16:48:53 -0800

    el_after

---
+++ get_el_after.js
@@ -0,0 +1,11 @@
+// Write a function called "getElementsAfter".
+//
+// Given an array and an index, "getElementsAfter" returns a new array with all the elements after (but not including) the given index.
+//
+// var output = getElementsAfter(['a', 'b', 'c', 'd', 'e'], 2);
+// console.log(output); // --> ['d', 'e']
+//
+// Starter Code :
+// function getElementsAfter(array, n) {
+//   // your code here
+// }
