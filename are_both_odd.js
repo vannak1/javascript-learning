20b8e0921f1b862ceeb1eba86c743ab063d2e966
Vannak Chhay <vannak.chhay1@gmail.com>
2017-01-22 16:47:53 -0800

    bothodd

---
+++ are_both_odd.js
@@ -0,0 +1,11 @@
+// Write a function called "areBothOdd".
+//
+// Given 2 numbers, "areBothOdd" returns whether or not both of the given numbers are odd.
+//
+// var output = areBothOdd(1, 3);
+// console.log(output); // --> true
+//
+// Starter Code :
+// function areBothOdd(num1, num2) {
+//   // your code here
+// }
