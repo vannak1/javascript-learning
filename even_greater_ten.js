e54fe7b8f9a573d482bd9783c03273294801dca5
Vannak Chhay <vannak.chhay1@gmail.com>
2017-01-22 16:34:53 -0800

    even_greater

---
+++ even_greater_ten.js
@@ -0,0 +1,11 @@
+// Write a function called "isEvenAndGreaterThanTen".
+//
+// Given a number, "isEvenAndGreaterThanTen" returns whether it is both even and greater than 10.
+//
+// var output = isEvenAndGreaterThanTen(13);
+// console.log(output); // --> false
+//
+// Starter Code :
+// function isEvenAndGreaterThanTen(num) {
+//   // your code here
+// }
