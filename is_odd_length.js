66102e6650868d7a8534b817a32f1a76e142a5fc
Vannak Chhay <vannak.chhay1@gmail.com>
2017-01-22 16:48:53 -0800

    is_odd_length

---
+++ is_odd_length.js
@@ -0,0 +1,11 @@
+// Write a function called "isOddLength".
+//
+// Given a word, "isOddLength" returns whether the length of the given word is odd.
+//
+// var output = isOddLength('special');
+// console.log(output); // --> true
+//
+// Starter Code :
+// function isOddLength(word) {
+//   // your code here
+// }
