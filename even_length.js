5fd460ce43cde8d3a2baea085b2592bf55cb6119
Vannak Chhay <vannak.chhay1@gmail.com>
2017-01-22 16:40:53 -0800

    even_length

---
+++ even_length.js
@@ -0,0 +1,11 @@
+// Write a function called "isEvenLength".
+//
+// Given a word, "isEvenLength" returns whether the length of the word is even.
+//
+// var output = isEvenLength('wow');
+// console.log(output); // --> false
+//
+// Starter Code :
+// function isEvenLength(word) {
+//   // your code here
+// }
