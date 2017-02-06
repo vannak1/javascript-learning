2cd0de913874b7eccb80466b5f15c0ce8f685efc
Vannak Chhay <vannak.chhay1@gmail.com>
2017-02-06 11:37:17 -0500

    added several problems

---
+++ countWords.js
@@ -0,0 +1,16 @@
+/*
+Write a function called "countWords".
+
+Given a string, "countWords" returns an object where each key is a word in the given string, with its value being how many times that word appeared in th given  string.
+
+Notes:
+* If given an empty string, it should return an empty object.
+
+var output = countWords('ask a bunch get a bunch');
+console.log(output); // --> {ask: 1, a: 2, bunch: 2, get: 1}
+
+Starter Code :
+function countWords(str) {
+  // your code here
+}
+*/
