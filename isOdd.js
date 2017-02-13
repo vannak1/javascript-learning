948ea57e814d3b91bfa933923e16afec69686599
Vannak Chhay <vannak.chhay1@gmail.com>
2017-02-12 16:34:53 -0800

    isOdd added

---
+++ isOdd.js
@@ -0,0 +1,11 @@
+// Write a function called "isOdd".
+//
+// Given a number, "isOdd" returns whether the given number is odd.
+//
+// var output = isOdd(9);
+// console.log(output); // --> true
+//
+// Starter Code :
+// function isOdd(num) {
+//   // your code here
+// }
