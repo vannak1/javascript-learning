3d8bc759a4cada26991934c10ddba395c0e18469
Vannak Chhay <vannak.chhay1@gmail.com>
2017-02-19 14:34:54 -0500

    added fullname property

---
+++ add_fullname.js
@@ -0,0 +1,18 @@
+// Write a function called "addFullNameProperty".
+//
+// Given an object that has a "firstName" property and a "lastName" property, "addFullNameProperty" returns a "fullName" property whose value is a string with the first name and last name separated by a space.
+//
+// var person = {
+//   firstName: 'Jade',
+//   lastName: 'Smith'
+// };
+// addFullNameProperty(person);
+// console.log(person.fullName); // --> 'Jade Smith'
+//
+// Starter Code :
+// function addFullNameProperty(obj) {
+//   // your code here
+// }
+function addFullNameProperty(obj) {
+  // your code here
+}
