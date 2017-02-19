ed7c62aa0a4174c66e8c75b66f927aa71907d0b7
Vannak Chhay <vannak.chhay1@gmail.com>
2017-02-19 14:36:55 -0500

    addToBack

---
+++ add_to_back.js
@@ -0,0 +1,17 @@
+// Write a function called "addToBack".
+//
+// Given an array and an element, "addToBack" returns the given array with the given element added to the end.
+//
+// Note: It should be the SAME array, not a new array.
+//
+// var output = addToBack([1, 2], 3);
+// console.log(output); // -> [1, 2, 3]
+//
+// Starter Code :
+// function addToBack(arr, element) {
+//   // your code here
+// }
+
+function addToBack(arr, element) {
+  // your code here
+}
