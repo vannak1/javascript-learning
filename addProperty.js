70a84ba738a7d391b6bbd038eb852e815cdd3d3e
Vannak Chhay <vannak.chhay1@gmail.com>
2017-02-19 14:36:18 -0500

    addProperty

---
+++ addProperty.js
@@ -0,0 +1,16 @@
+// Write a function called "addProperty".
+//
+// Given an object, and a key, "addProperty" sets a new property on the given object with a value of true.
+//
+// var myObj = {};
+// addProperty(myObj, 'myProperty');
+// console.log(myObj.myProperty); // --> true
+//
+// Starter Code :
+// function addProperty(obj, key) {
+//   // your code here
+// }
+
+function addProperty(obj, key) {
+  // your code here
+}
