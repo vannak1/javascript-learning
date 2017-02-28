1e9cd52780c2c03d1928649aed7d9256fb088a43
Vannak Chhay <vannak.chhay1@gmail.com>
2017-02-27 22:49:19 -0500

    billtotal function

---
+++ calculate_bill_total.js
@@ -0,0 +1,19 @@
+// Write a function called "calculateBillTotal".
+//
+// Given the pre tax and pre tip amount of a meal, "calculateBillTotal" returns the total amount due after tax and tip.
+//
+// Notes:
+// * Assume that sales tax is 9.5% and tip is 15%.
+// * Do NOT tip on the sales tax, only on the pre tip amount.
+//
+// var output = calculateBillTotal(20);
+// console.log(output); // --> 24.9
+//
+// Starter Code :
+// function calculateBillTotal(preTaxAndTipAmount) {
+//   // your code here
+// }
+
+function calculateBillTotal(preTaxAndTipAmount) {
+  // your code here
+}
