3d9080a2d55bae90b85e0c801c3a6edf790d2e51
Vannak Chhay <vannak.chhay1@gmail.com>
2017-02-27 22:51:21 -0500

    lengthofword

---
+++ getlenghtofword.js
@@ -0,0 +1,15 @@
+// Write a function called "getLengthOfWord".
+//
+// Given a word, "getLengthOfWord" returns the length of the given word.
+//
+// var output = getLengthOfWord('some');
+// console.log(output); // --> 4
+//
+// Starter Code :
+// function getLengthOfWord(word) {
+//   // your code here
+// }
+
+function getLengthOfWord(word) {
+  // your code here
+}
