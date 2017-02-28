481f42c331d82dcb48d5f8a0c9500dfbfadb763f
Vannak Chhay <vannak.chhay1@gmail.com>
2017-02-27 22:51:03 -0500

    getfullname

---
+++ getfullname.js
@@ -0,0 +1,15 @@
+// Write a function called "getFullName".
+//
+// Given a first and a last name, "getFullName" returns a single string with the given first and last names separated by a single space.
+//
+// var output = getFullName('Joe', 'Smith');
+// console.log(output); // --> 'Joe Smith'
+//
+// Starter Code :
+// function getFullName(firstName, lastName) {
+//   // your code here
+// }
+
+function getFullName(firstName, lastName) {
+  // your code here
+}
