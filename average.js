f86a9c379804a6c29e4b2623a2a295950e43faf1
Vannak Chhay <vannak.chhay1@gmail.com>
2017-02-27 22:48:35 -0500

    average

---
+++ average.js
@@ -0,0 +1,15 @@
+// Write a function called "average".
+//
+// Given two numbers, "average" returns their average.
+//
+// var output = average(4, 6);
+// console.log(output); // --> 5
+//
+// Starter Code :
+// function average(num1, num2) {
+//   // your code here
+// }
+
+function average(num1, num2) {
+  // your code here
+}
