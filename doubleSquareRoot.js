174c9ea5ce8d81086017dc6214acd6f89508d916
Vannak Chhay <vannak.chhay1@gmail.com>
2017-03-27 11:07:11 -0400

    double square root

---
+++ doubleSquareRoot.js
@@ -0,0 +1,11 @@
+// Write a function called "doubleSquareRootOf".
+//
+// Given a number, "doubleSquareRootOf" returns double its square root.
+//
+// var output = doubleSquareRootOf(121);
+// console.log(output); // --> 22
+//
+// Starter Code :
+function doubleSquareRootOf(num) {
+  // your code here
+}
